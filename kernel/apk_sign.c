b8d4eb69c01792f4a5de0893c52d62be50bd3aa7
Lampion <saviopio03@gmail.com>
2026-02-07 10:14:40 +0700

    Fix syntax for RKSU signature check
    Signed-off-by: Lampion <saviopio03@gmail.com>

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -372,5 +372,5 @@ bool is_manager_apk(char *path)
 	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588") //KOWSU
 	|| check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549") //xxKSU
 	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588") //KOWSU
-	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b") //RKSU
+	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b"); //RKSU
 }
